f25ce2cbbd9970ee92750fd754e26d53749e3215
wbw88 <94088961+wbw88@users.noreply.github.com>
2021-11-14 23:06:16 -0800

    update commit

--- assets/mail.php
+++ assets/mail.php
@@ -3,7 +3,7 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$formcontent="From: $name \n Message: $message";
-	$recipient = "nathanang2000@gmail.com";
+	$recipient = "info@lumatas.com";
 	$subject = "Contact Form";
 	$mailheader = "From: $email \r\n";
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
