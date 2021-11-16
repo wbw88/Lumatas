022ce9defc88cc435337151a3192e6010521db3e
wbw88 <94088961+wbw88@users.noreply.github.com>
2021-11-15 22:39:31 -0800

    Update mail.php

--- assets/mail.php
+++ assets/mail.php
@@ -3,7 +3,7 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$formcontent="From: $name \n Message: $message";
-	$recipient = "info@lumatas.com";
+	$recipient = "kf6ios@yahoo.com";
 	$subject = "Contact Form";
 	$mailheader = "From: $email \r\n";
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
